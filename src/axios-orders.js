245e13a18e906a2a36804b4767866994ff68bc01
MustafaGurbuz <beatnikmustafa@hotmail.com>
2021-02-28 19:22:37 +0300

    some changes made axios-orders file

--- src/axios-orders.js
+++ src/axios-orders.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const instance = axios.create({
-    baseURL: 'https://burger-project-reactjs-552df-default-rtdb.firebaseio.com/'
+    baseURL: 'YOUR_FIREBASE_URL'
 });
 
 export default instance;
